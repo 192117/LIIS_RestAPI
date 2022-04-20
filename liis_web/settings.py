d75761539b22e5b08483afdafd36bd6f3141a4f8
Kokoc <noti1996@gmail.com>
2022-04-20 23:51:02 +0300

    Final commit5.

--- liis_web/settings.py
+++ liis_web/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # DEBUG = True
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True) )
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['whispering-escarpment-44420.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
